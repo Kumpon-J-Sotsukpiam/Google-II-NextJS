1eee3833e5a5ad5b234d9ad0a2758d2a981691a8
kumpon J Sotsukpiam <kumpon_s@brainergy.digital>
2022-03-12 12:55:19 +0700

    update: home page google is done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon } from '@heroicons/react/outline'
 
 import Avatar from '../components/Avatar';
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-center h-screen'>
       <Head>
         <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
@@ -23,6 +26,7 @@ const Home: NextPage = () => {
           <p className='link'>Gmail</p>
           <p className='link'>Images</p>
           {/* icon */}
+          <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
           {/* avatar */}
           <Avatar
             url='https://lh3.googleusercontent.com/a-/AOh14GhxFrZThqhiAf90gcAj8gnNdOpWW0Gphr7JuunJ0Sg=s288-p-rw-no'
@@ -30,7 +34,34 @@ const Home: NextPage = () => {
         </div>
       </header>
       {/* body */}
+      <form className='flex flex-col items-center mt-44 flex-grow'>
+        <Image
+          src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+          width={300}
+          height={100}
+        />
+        <div
+          className='flex w-full mt-5 hover:shadow-lg 
+            focus-within:shadow-lg max-w-md rounded-full 
+            border border-gray-200
+            px-5 py-3 items-center
+            sm:max-w-xl
+            lg:max-w-2xl'
+        >
+          <SearchIcon className='h-5 mr-3 text-gray-500' />
+          <input type="text" className='flex-grow focus:outline-none' />
+          <MicrophoneIcon className='h-5' />
+        </div>
+        <div
+          className='flex flex-col w-1/2 space-y-2 justify-center mt-8 
+          sm:space-y-0 sm:flex-row sm:space-x-4'
+        >
+          <button className='btn'>Google Search</button>
+          <button className='btn'>I'm Feeling Lucky</button>
+        </div>
+      </form>
       {/* footer */}
+      <Footer />
     </div>
   )
 }
